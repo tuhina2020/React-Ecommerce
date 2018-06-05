69a5e171b1c30e73c57d4767559ffc65ea7a3c05
tuhina2020 <tuhinatwyla@gmail.com>
2018-06-05 21:34:37 +0530

    update README

--- README.md
+++ README.md
@@ -4,3 +4,26 @@ E-Commerce Website
 Filter through items from own API, add them to your cart, and see similar items.
 
 ![demo](ecommerce.gif)
+
+Instructions
+
+steps to run the website
+
+1. install pm2
+
+	`npm install pm2 -g`
+
+
+2. install packages
+
+	`npm install`
+
+
+3. Build the files
+	`npm run build:prod`
+
+
+4. Start pm2 process
+	`pm2 run prod.json`
+
+5. open the site on http://localhost:4040
--- src/components/BaseLayout/components/Header/index.js
+++ src/components/BaseLayout/components/Header/index.js
@@ -8,11 +8,8 @@ const Header = () => (
   <div className="header">
     <h1 id="header-title">Shop Now</h1>
     <div className="links-header">
-      <p><NavLink activeClassName="selected" className="nav-link-header" to="/designers">Designers</NavLink></p>
-      <p><NavLink activeClassName="selected" className="nav-link-header" to="/women">Women</NavLink></p>
-      <p><NavLink activeClassName="selected" className="nav-link-header" to="/men">Men</NavLink></p>
-      <p><NavLink activeClassName="selected" className="nav-link-header" to="/clothes">Clothes</NavLink></p>
-      <p><NavLink activeClassName="selected" className="nav-link-header" to="/accessories">Accessories</NavLink></p>
+      <p><NavLink activeClassName="selected" className="nav-link-header" to="/designers">DESIGNERS</NavLink></p>
+      <p><NavLink activeClassName="selected" className="nav-link-header" to="/women">COLLECTION</NavLink></p>
     </div>
   </div>
 )
