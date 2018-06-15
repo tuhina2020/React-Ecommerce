743b383c7611f5fd8969bf678e22f585d0fc4cc6
tuhina2020 <tuhinatwyla@gmail.com>
2018-06-16 02:19:21 +0530

    remove unneccessary header and footer

--- src/components/Main.js
+++ src/components/Main.js
@@ -16,16 +16,16 @@ export default class Main extends React.Component {
 
 	render () {
 		return (<BrowserRouter>
-	    <BaseLayout>
-	      <Switch>
-	        <Route exact path="/" component={App} />
-	        <Route path="/accessories" component={Accessories} />
-	        <Route exact path="/designers/:id" component={ShowDesigner} />
-	        <Route path="/designers" component={Designers} />
-	        <Route exact path="/products/:id" component={ShowProduct} />
-	        <Route path="/products" component={Products} />
-	      </Switch>
-	    </BaseLayout>
+		  <BaseLayout>
+		      <Switch>
+		        <Route exact path="/" component={App} />
+		        <Route path="/accessories" component={Accessories} />
+		        <Route exact path="/designers/:id" component={ShowDesigner} />
+		        <Route path="/designers" component={Designers} />
+		        <Route exact path="/products/:id" component={ShowProduct} />
+		        <Route path="/products" component={Products} />
+		      </Switch>
+	      </BaseLayout>
 	  </BrowserRouter>);
 	}
 };
